19936bb259d05567b2230ec10dd7955ceb62b9b2
sephelan <147577380+sephelan@users.noreply.github.com>
2025-04-04 10:04:34 -0500

    Create main.py
    This runs a reandom model with reward score printed with each move printout.

---
+++ main.py
@@ -0,0 +1,15 @@
+import gymnasium as gym
+
+from tetris_gymnasium.envs.tetris import Tetris
+
+if __name__ == "__main__":
+    env = gym.make("tetris_gymnasium/Tetris", render_mode="ansi")
+    env.reset(seed=42)
+
+    terminated = False
+    while not terminated:
+        print(env.render() + "\n")
+        action = env.action_space.sample()
+        observation, reward, terminated, truncated, info = env.step(action)
+        print(reward)
+    print("Game Over!")
